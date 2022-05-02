ed7e3925dadc0d272a19bd49c97602441c8234be
neto schmitt <agneloschmittneto@gmail.com>
2022-05-02 19:43:01 -0300

    componentes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,19 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
+    node: true,
   },
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
-    'airbnb',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaFeatures: {
@@ -15,12 +23,15 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react', 'prettier', 'react-hooks'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
-    'react/jsx-filename-extension': 0,
-    'react/state-in-constructor': 0,
-    'react/forbid-prop-types': 0,
+    'react/jsx-filename-extension':0,
+    'react/react-in-jsx-scope': 'off',
+    'react/state-in-constructor': 0
   },
 };
